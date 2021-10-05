412de21c786f8e737e928a7621ded677b9c2b86b
HttpKiwi <juan.avenia@correounivalle.edu.co>
2021-10-05 04:25:24 -0500

    Adding calendar and making changes to css

--- apps/calendario/urls.py
+++ apps/calendario/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.urls.conf import re_path
 from . import views
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('', views.actividades),
     path('actividades/', views.actividades, name="actividades"),
     path('crear-actividad/', views.crear_actividad, name="crear-actividad"),
-    path('editar-actividad/<str:pk>/', views.editar_actividad, name="editar-actividad")
+    path('editar-actividad/<str:pk>/', views.editar_actividad, name="editar-actividad"),
+    re_path(r'^swingtime/events/type/([^/]+)/$', views.event_type, name='karate-event'),
+    path('swingtime/', include('swingtime.urls')),
 ]
--- apps/calendario/views.py
+++ apps/calendario/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from .forms import ActividadForm, EditarActividadForm, CrearCalendarioForm
 from .models import Actividad
+from datetime import datetime, timedelta
+from django.template.context import RequestContext
+from django.shortcuts import get_object_or_404, render
+
+from swingtime import models as swingtime
 
 
 def actividades(request):
@@ -49,4 +54,18 @@ def crear_calendario(request):
         "form": CrearCalendarioForm
     }
     return render(request, "actividad.html", context)
+
+
+def event_type(request, abbr):
+    event_type = get_object_or_404(swingtime.EventType, abbr=abbr)
+    now = datetime.now()
+    occurrences = swingtime.Occurrence.objects.filter(
+        event__event_type=event_type,
+        start_time__gte=now,
+        start_time__lte=now+timedelta(days=+30)
+    )
+    return render(request, 'upcoming_by_event_type.html', {
+        'occurrences': occurrences,
+        'event_type': event_type
+    })
 
